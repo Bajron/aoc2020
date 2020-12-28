4691fc14b074e80caba53d52f719c3e0d81f2040
btee <b.kramarski@gmail.com>
2020-12-28 15:28:21 +0100

    Day 1

--- day1/main.cpp
+++ day1/main.cpp
@@ -1,15 +1,33 @@
 #include <iostream>
-#include <vector>
+#include <tuple>
 #include <unordered_set>
+#include <utility>
+#include <vector>
 
-std::pair<long,long> summingTo(const std::vector<long>& input, const long target) {
+std::pair<long, long> summingTo(const std::vector<long> &input,
+                                const long target) {
     std::unordered_set<long> hash{input.begin(), input.end()};
     for (auto number : input) {
-        if (hash.find(target - number) != hash.end()) {
-            return {number, target};
+        const auto matching = target - number;
+        if (hash.find(matching) != hash.end()) {
+            return {number, matching};
         }
     }
-    return {-1, -1};
+    return {0, 0};
+}
+
+std::tuple<long, long, long> threeSummingTo(const std::vector<long> &input,
+                                            const long target) {
+    std::unordered_set<long> hash{input.begin(), input.end()};
+    for (std::size_t i = 0; i < input.size(); ++i) {
+        for (std::size_t j = i; j < input.size(); ++j) {
+            const auto matching = target - (input[i] + input[j]);
+            if (hash.find(matching) != hash.end()) {
+                return {input[i], input[j], matching};
+            }
+        }
+    }
+    return {0, 0, 0};
 }
 
 int main(int argc, char args[]) {
@@ -20,12 +38,17 @@ int main(int argc, char args[]) {
         if (std::cin.good()) {
             input.push_back(number);
         }
-    } while(std::cin.good());
+    } while (std::cin.good());
 
     auto p = summingTo(input, 2020);
 
     std::clog << "Found: " << p.first << " + " << p.second << std::endl;
     std::cout << p.first * p.second << std::endl;
+
+    auto t = threeSummingTo(input, 2020);
+    std::clog << "Found: " << std::get<0>(t) << " + " << std::get<1>(t) << " + "
+              << std::get<2>(t) << std::endl;
+    std::cout << std::get<0>(t) * std::get<1>(t) * std::get<2>(t) << std::endl;
 
     return 0;
 }
